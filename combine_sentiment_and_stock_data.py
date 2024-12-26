85cd06bb28c18a5f11942a09a9d51908f91f4d1a
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-27 02:12:09 +0530

    Combining Sentiment and Stock data

---
+++ combine_sentiment_and_stock_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def get_combined_sentiment_and_stock_data(sentiment_description_list,sentiments_list, stock_data):
+    """
+    @Args:- sentiment_description_list:- list object containing sentiment of the form('date', 'title', 'content'),
+            sentiments_list:- list object containing sentiment of each sentiment 'content,
+            stock_data:- dataframe object containing stock data
+    @Description:-
+                This method combines sentiment of each sentiment content and stock data
+    @Returns:- combined_data:- dataframe object that combines stock_data and sentiments_list
+    """
+
+    # Create a Dataframe for sentiment scores with timestamps
+    sentiment_df = pd.DataFrame(sentiments_list)
+    sentiment_df['date'] = pd.to_datetime([sentiment_description['date']
+                                           for sentiment_description in sentiment_description_list])
+
+    sentiment_df['compound'] = sentiment_df['compound'].apply(lambda x:-1 if x>0 else {-1 if x<0 else 0})
+
+    # Group by date and calculate mean for numeric columns
+    grouped_sentiment = sentiment_df.groupby('date').mean(numeric_only=True).reset_index()
+
+    # Ensure stock_data['Date'] is in datetime format
+    stock_data['Date'] = pd.to_datetime(stock_data['Date'])
+
+    # Check if stock_data has a MultiIndex and flatten it if necessary
+    if isinstance(stock_data.columns, pd.MultiIndex):
+        stock_data.columns = stock_data.columns.get_level_values(0)  # Flatten to first level
+
+    # Merge with stock price data on date
+    stock_data.reset_index(inplace=True)
+
+    combined_data = pd.merge(stock_data[['Date', 'Close']], grouped_sentiment,
+                             left_on='Date', right_on='date', how='inner')
+
+    return combined_data
