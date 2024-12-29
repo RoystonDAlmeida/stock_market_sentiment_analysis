a70fbb1301dc0228db0077e32a64b26582643725
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-12-29 23:04:11 +0530

    Added sentiment values to each article dict

--- fetch_sentiment_data.py
+++ fetch_sentiment_data.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
+import calendar
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -22,37 +23,65 @@ def fetch_sentiment_data(ticker):
     @Returns:- description_list:- list object containing all descriptions for the ticker
     """
 
-    response = requests.get(f"{api_url}?s={ticker.upper()}&api_token={api_key}&fmt=json")
+    # Parse the start date
+    start = datetime.strptime('2021-01-01', '%Y-%m-%d')
 
-    if response.status_code == 200:
-        news_data = response.json()
+    # Get yesterday's date(since training data should be upto previous date)
+    yesterday = datetime.today() - timedelta(days=1)
 
-        for article in news_data:
+    # Initialize current date to the first day of the start month
+    current_date = start.replace(day=1)
 
-            # Convert date format from '2024-12-22T12:00:00+00:00' to '23 Aug 2024'
-            original_date = article['date']
+    while current_date <= yesterday:
+        print(current_date, yesterday)
+        # Get the last day of the current month
+        last_day = calendar.monthrange(current_date.year, current_date.month)[1]
 
-            # Fixing the timezone format if necessary
-            if original_date.endswith('+00:0'):
-                original_date = original_date[:-1] + '00'  # Change '+00:0' to '+00:00'
+        # Format the start and end dates
+        start_of_month = current_date.strftime("%Y-%m-%d")
+        end_of_month = current_date.replace(day=last_day).strftime("%Y-%m-%d")
 
-            # Convert to datetime and format as needed
-            try:
-                formatted_date = datetime.fromisoformat(original_date).strftime('%d %b %Y')
-            except ValueError as e:
-                print(f"Error parsing date '{original_date}': {e}")
-                continue  # Skip this article if there's an error
+        # Move to the first day of the next month
+        if current_date.month == 12:
+            current_date = current_date.replace(year=current_date.year + 1, month=1)
+        else:
+            current_date = current_date.replace(month=current_date.month + 1)
 
-            # Create a dictionary for each article with formatted date, title, and content
-            article_object = {
-                "date": formatted_date,
-                "title": article['title'],
-                "content": article['content']
-            }
+        response = requests.get(f"{api_url}?s={ticker.upper()}&from={start_of_month}&to={end_of_month}&limit=1000&api_token={api_key}&fmt=json")
 
-            description_list.append(article_object)
+        if response.status_code == 200:
+            news_data = response.json()
+            print(f"Fetched financial sentiment data from {start_of_month} to {end_of_month}")
 
-    else:
-        print(f"Failed to retrieve data: {response.status_code}")
+            for article in news_data:
+
+                # Convert date format from '2024-12-22T12:00:00+00:00' to '23 Aug 2024'
+                original_date = article['date']
+
+                # Fixing the timezone format if necessary
+                if original_date.endswith('+00:0'):
+                    original_date = original_date[:-1] + '00'  # Change '+00:0' to '+00:00'
+
+                # Convert to datetime and format as needed
+                try:
+                    formatted_date = datetime.fromisoformat(original_date).strftime('%d %b %Y')
+                except ValueError as e:
+                    print(f"Error parsing date '{original_date}': {e}")
+                    continue  # Skip this article if there's an error
+
+                if article.get('sentiment') is not None:
+                    # 'sentiment' value should not be None
+                    # Create a dictionary for each article with formatted date, title,content and sentiment
+                    article_object = {
+                        "date": formatted_date,
+                        "title": article['title'],
+                        "content": article['content'],
+                        "sentiment": article['sentiment']
+                    }
+
+                    description_list.append(article_object)
+
+        else:
+            print(f"Failed to retrieve data: {response.status_code}")
 
     return description_list
